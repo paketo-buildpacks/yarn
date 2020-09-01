12d6e83705fabcd0543843e440e1d3a2443407c7
Tim Hitchener <thitchener@pivotal.io>
2020-09-01 09:22:12 -0400

    Remove entry.Version

--- plan_refinery.go
+++ plan_refinery.go
@@ -13,14 +13,14 @@ func NewPlanRefinery() PlanRefinery {
 
 func (r PlanRefinery) BillOfMaterials(dependency postal.Dependency) packit.BuildpackPlanEntry {
 	return packit.BuildpackPlanEntry{
-		Name:    dependency.ID,
-		Version: dependency.Version,
+		Name: dependency.ID,
 		Metadata: map[string]interface{}{
 			"licenses": []string{},
 			"name":     dependency.Name,
 			"sha256":   dependency.SHA256,
 			"stacks":   dependency.Stacks,
 			"uri":      dependency.URI,
+			"version":  dependency.Version,
 		},
 	}
 }

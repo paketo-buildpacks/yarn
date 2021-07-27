7d032e5d002474bb21679553185325c02e3e6fea
Forest Eckhardt <feckhardt@pivotal.io>
2021-07-27 10:12:54 -0400

    Update build to use GenerateBillOfMaterials `postal` function (#135)
    - Removes PlanRefinery
    - Removes YarnLogger in favor of the scribe.Emitter which covers all of
    the same functionalities
    - Removes the PlanEntryResolver in favor of a draft.Planner which covers
    the same functionality with a nil priority plan as well as giving access
    to the MergeLayerType function
    - Updates to using postal.Deliver as postal.Install is deprecated

--- build.go
+++ build.go
@@ -7,30 +7,27 @@ import (
 	"github.com/paketo-buildpacks/packit"
 	"github.com/paketo-buildpacks/packit/chronos"
 	"github.com/paketo-buildpacks/packit/postal"
+	"github.com/paketo-buildpacks/packit/scribe"
 )
 
 //go:generate faux --interface EntryResolver --output fakes/entry_resolver.go
 type EntryResolver interface {
-	Resolve([]packit.BuildpackPlanEntry) packit.BuildpackPlanEntry
+	Resolve(name string, entries []packit.BuildpackPlanEntry, priorites []interface{}) (packit.BuildpackPlanEntry, []packit.BuildpackPlanEntry)
+	MergeLayerTypes(name string, entries []packit.BuildpackPlanEntry) (launch, build bool)
 }
 
 //go:generate faux --interface DependencyManager --output fakes/dependency_manager.go
 type DependencyManager interface {
 	Resolve(path, id, version, stack string) (postal.Dependency, error)
-	Install(dependency postal.Dependency, cnbPath, layerPath string) error
-}
-
-//go:generate faux --interface BuildPlanRefinery --output fakes/build_plan_refinery.go
-type BuildPlanRefinery interface {
-	BillOfMaterials(postal.Dependency) packit.BuildpackPlanEntry
+	Deliver(dependency postal.Dependency, cnbPath, layerPath, platformPath string) error
+	GenerateBillOfMaterials(dependencies ...postal.Dependency) []packit.BOMEntry
 }
 
 func Build(
-	entries EntryResolver,
-	dependencies DependencyManager,
-	planRefinery BuildPlanRefinery,
+	entryResolver EntryResolver,
+	dependencyManager DependencyManager,
 	clock chronos.Clock,
-	logger LogEmitter,
+	logger scribe.Emitter,
 ) packit.BuildFunc {
 	return func(context packit.BuildContext) (packit.BuildResult, error) {
 		logger.Title("%s %s", context.BuildpackInfo.Name, context.BuildpackInfo.Version)
@@ -40,30 +37,46 @@ func Build(
 			return packit.BuildResult{}, err
 		}
 
-		entry := entries.Resolve(context.Plan.Entries)
-		entryVersion, _ := entry.Metadata["version"].(string)
+		entry, _ := entryResolver.Resolve("yarn", context.Plan.Entries, nil)
+		version, ok := entry.Metadata["version"].(string)
+		if !ok {
+			version = "default"
+		}
 
-		dependency, err := dependencies.Resolve(
+		dependency, err := dependencyManager.Resolve(
 			filepath.Join(context.CNBPath, "buildpack.toml"),
 			entry.Name,
-			entryVersion,
+			version,
 			context.Stack)
 		if err != nil {
 			return packit.BuildResult{}, err
 		}
 
-		bom := planRefinery.BillOfMaterials(dependency)
+		bom := dependencyManager.GenerateBillOfMaterials(dependency)
+
+		launch, build := entryResolver.MergeLayerTypes("yarn", context.Plan.Entries)
+
+		var buildMetadata = packit.BuildMetadata{}
+		var launchMetadata = packit.LaunchMetadata{}
+		if build {
+			buildMetadata = packit.BuildMetadata{BOM: bom}
+		}
+
+		if launch {
+			launchMetadata = packit.LaunchMetadata{BOM: bom}
+		}
 
 		cachedSHA, ok := yarnLayer.Metadata[DependencyCacheKey].(string)
 		if ok && cachedSHA == dependency.SHA256 {
 			logger.Process("Reusing cached layer %s", yarnLayer.Path)
 			logger.Break()
 
+			yarnLayer.Launch, yarnLayer.Build, yarnLayer.Cache = launch, build, build
+
 			return packit.BuildResult{
-				Plan: packit.BuildpackPlan{
-					Entries: []packit.BuildpackPlanEntry{bom},
-				},
 				Layers: []packit.Layer{yarnLayer},
+				Build:  buildMetadata,
+				Launch: launchMetadata,
 			}, nil
 		}
 
@@ -74,14 +87,12 @@ func Build(
 			return packit.BuildResult{}, err
 		}
 
-		yarnLayer.Build = entry.Metadata["build"] == true
-		yarnLayer.Cache = entry.Metadata["build"] == true
-		yarnLayer.Launch = entry.Metadata["launch"] == true
+		yarnLayer.Launch, yarnLayer.Build, yarnLayer.Cache = launch, build, build
 
 		logger.Subprocess("Installing Yarn")
 
 		duration, err := clock.Measure(func() error {
-			return dependencies.Install(dependency, context.CNBPath, yarnLayer.Path)
+			return dependencyManager.Deliver(dependency, context.CNBPath, yarnLayer.Path, context.Platform.Path)
 		})
 		if err != nil {
 			return packit.BuildResult{}, err
@@ -96,10 +107,9 @@ func Build(
 		}
 
 		return packit.BuildResult{
-			Plan: packit.BuildpackPlan{
-				Entries: []packit.BuildpackPlanEntry{bom},
-			},
 			Layers: []packit.Layer{yarnLayer},
+			Build:  buildMetadata,
+			Launch: launchMetadata,
 		}, nil
 	}
 }
